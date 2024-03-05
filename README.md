d2634458acd0814f5079b4d22587e09ffda84a06
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-03-06 00:19:22 +0530

    added tailwindcss and some text animation

--- README.md
+++
@@ -1,2 +0,0 @@
-# y4sh.in
-My personal portfolio website trying to made while learning react at the same time also be using tailwind css for styling !!
--- y4sh.in/src/App.jsx
+++ y4sh.in/src/App.jsx
@@ -1,11 +1,35 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [text, setText] = useState("");
+  const phrases = ["y4sh coming soon ...."];
+  const speed = 100; // Adjust typing speed (milliseconds)
+
+  useEffect(() => {
+    let currentIndex = 0;
+    let currentText = "";
+    const interval = setInterval(() => {
+      currentText += phrases[0][currentIndex];
+      setText(currentText);
+      currentIndex++;
+      if (currentIndex === phrases[0].length) {
+        clearInterval(interval);
+        setTimeout(() => {
+          setText("");
+          currentIndex = 0;
+        }, 1000); // Adjust the delay before starting the next typing
+      }
+    }, speed);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <>
-      <h3>y4sh.in coming soon ..</h3>
-    </>
+    <div className="center">
+      <h1 className="text-white">{text}</h1>
+    </div>
   );
-}
+};
 
 export default App;
