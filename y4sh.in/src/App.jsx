8e0f1d623e367a17db54ac47c4d2366df248da7e
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-03-16 00:43:47 +0530

    changes

--- y4sh.in/src/App.jsx
+++ y4sh.in/src/App.jsx
@@ -3,13 +3,16 @@ import "./App.css";
 
 function App() {
   return (
-    <div id="main" className=" h-screen w-screen text-center bg-[url(/src/assets/vagabond.png)] bg-center">
-      <h1 className="pt-2 text-font-thin text-2xl font-fira text-black ">
-        This is yash, y4sh coming soon .......
-      </h1>
-    </div>
+    <>
+      <div
+        id="main"
+        className=" h-screen w-screen text-center bg-[url(/src/assets/vagabond.png)] bg-center text-font-thin text-2xl font-fira text-black"
+      >
+        <h1>y4sh</h1>
+        <h1>Programmer || Gamer || Cirtic</h1>
+      </div >
+    </>
   );
 }
-
 
 export default App;
