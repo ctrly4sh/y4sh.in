7e080e3a7ab0ee9c995cc5a9e3f0e68654fb9edc
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-03-18 12:56:19 +0530

    minor

---
+++ y4sh.in/src/navbar.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="">
+        <li>Projects</li>
+        <li>yash</li>
+        <li>
+          human
+          </li>
+      </nav>
+    </>
+  );
+}
+export default Navbar
