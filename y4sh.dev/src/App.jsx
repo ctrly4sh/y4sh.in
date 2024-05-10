ca2d82119c288fe8b46e1b39e85aa8f4597f07a9
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-05-10 22:49:11 +0530

    card

--- y4sh.dev/src/App.jsx
+++ y4sh.dev/src/App.jsx
@@ -1,27 +1,16 @@
-import Navbar from "./Navbar"
-import "./index.css"
-const github = "https://github.com/ctrly4sh"
-const medium = "https://medium.com"
-
-function App() {
+const App = () => {
   return (
-    <>
-      <Navbar />
-      <div className="bg-font-fira text-center">
-        <div id="main-content">
-          <p className="text-2xl">
-            hii this is yash here, <br />
-            I write <a className="font-pixelify"  href={github} target="_blank">Code</a> and i <a className="font-pixelify"  href="#projects">Build</a> stuff with it.
-          </p>
-          <div id="keywords">
-            <a className="hover:" href={github}>Programmer</a> ,
-            <a href={github}> Open Source Contributor ,</a>
-            <a href={medium}> Tech enthusiast.</a>
-          </div>
-        </div>
+    <div className="card">
+      <img src="https://i.pinimg.com/736x/d1/15/0c/d1150cc4e1f8c7111cd8c03aa949b782.jpg" alt="" width={150} height={150} />
+      <h2>y4sh</h2>
+      <p>I write Code and i build stuff with it</p>
+      <div className="keywords">
+        <span><a href="#">Programmer</a></span>
+        <span><a href="#">Open Source Contributor</a></span> 
+        <span><a href="#">Tech Enth.</a></span>
       </div>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
